87f0a64bc7334bfaae24c73f8581625082258bc2
austin-michael <austin.michaelwill@gmail.com>
2020-02-02 19:34:01 -0700

    List of common misspellings is now stored in a data structure and running against algorithm

--- minmum_edit_distance_DP.py
+++ minmum_edit_distance_DP.py
@@ -47,4 +47,42 @@ def unit_tests():
     print('Minimum Edit Distance of sequence one =', sequence_one_3, ' and sequence two =', sequence_two_3, ' is ', minimum_edit_distance_DP(n_3, m_3, sequence_one_3, sequence_two_3))
 
 
-unit_tests()
+def read_list_of_common_misspellings():
+    common_misspellings_arr = []
+    incorrect_spellings = []
+    correct_spellings = []
+    common_misspellings_dict = {}
+
+    with open('list_of_common_misspellings.txt') as CM:
+        common_misspellings_arr = CM.read().splitlines()
+
+    for i in range(common_misspellings_arr.__len__()):
+        incorrect_spellings.append(common_misspellings_arr[i].split('->')[0])
+        correct_spellings.append(common_misspellings_arr[i].split('->')[1])
+
+    for i in range(correct_spellings.__len__()):
+        correct_spellings[i] = correct_spellings[i].split(', ')
+
+    for i in range(incorrect_spellings.__len__()):
+        common_misspellings_dict[incorrect_spellings[i]] = correct_spellings[i]
+
+    return common_misspellings_dict
+
+
+def test_list_of_common_misspellings():
+    common_misspellings_dict = read_list_of_common_misspellings()
+
+    print(common_misspellings_dict)
+
+    for incorrect_word in common_misspellings_dict:
+        for correct_word in common_misspellings_dict[incorrect_word]:
+            n = incorrect_word.__len__()
+            m = correct_word.__len__()
+            A = incorrect_word
+            B = correct_word
+            print('Key: ', incorrect_word, '    Value: ', correct_word, '      MED: ', minimum_edit_distance_DP(n, m, A, B))
+
+
+
+# unit_tests()
+test_list_of_common_misspellings()
